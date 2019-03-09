e2aaee2ae1ea96ff3db36b997b0dba919d2d10d5
Khang Nguyen <mkavo92@gmail.com>
2019-03-09 13:17:26 -0600

    deploy

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    DB_URL: process.env.DB_URL,
+    DB_URL: 'mongodb://hackathon:minhkhang1@ds237955.mlab.com:37955/hackathon-project',
     SECRET: process.env.SECRET,
     AWS_KEY_ID: process.env.AWS_KEY_ID,
     AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
